108c19db39cfb94620c85472640add05fceb5399
Sara <saramf240614@gmail.com>
2025-04-17 14:49:43 -0400

    Creado el orquestador index.js. Ahora vamos por: gestionar los estados de interfaz(carga, error y éxito) e incluir la ruleta de carga

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import { showAdsController } from "./show-ads/showAdsController.js"
 
-showAdsController()
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector(".ads-container")
+    const showAdsButton = document.querySelector(".show-button")
+    showAdsButton.addEventListener("click", () => {
+        showAdsController(container)})
+})
